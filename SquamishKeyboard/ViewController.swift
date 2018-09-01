d96289205ace0e2e886ffc7fe284de4b88debc7e
Victor Temprano <victortemprano@victors-macbook-pro.local>
2018-09-01 17:36:27 +1000

    Initial Commit

---
+++ SquamishKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquamishKeyboard
+//
+//  Created by Victor Temprano on 2018-09-01.
+//  Copyright © 2018 Victor Temprano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
